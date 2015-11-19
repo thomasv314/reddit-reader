c195d7d5e8ee160ca4422b93ccc0cf4cd2ebcc02
Thomas Vendetta <thomas@thomasvendetta.com>
2015-11-19 18:34:06 -0500

    First commit, 6:30 PM

---
+++ config/apps.rb
@@ -0,0 +1,36 @@
+##
+# This file mounts each app in the Padrino project to a specified sub-uri.
+# You can mount additional applications using any of these commands below:
+#
+#   Padrino.mount('blog').to('/blog')
+#   Padrino.mount('blog', :app_class => 'BlogApp').to('/blog')
+#   Padrino.mount('blog', :app_file =>  'path/to/blog/app.rb').to('/blog')
+#
+# You can also map apps to a specified host:
+#
+#   Padrino.mount('Admin').host('admin.example.org')
+#   Padrino.mount('WebSite').host(/.*\.?example.org/)
+#   Padrino.mount('Foo').to('/foo').host('bar.example.org')
+#
+# Note 1: Mounted apps (by default) should be placed into the project root at '/app_name'.
+# Note 2: If you use the host matching remember to respect the order of the rules.
+#
+# By default, this file mounts the primary app which was generated with this project.
+# However, the mounted app can be modified as needed:
+#
+#   Padrino.mount('AppName', :app_file => 'path/to/file', :app_class => 'BlogApp').to('/')
+#
+
+##
+# Setup global project settings for your apps. These settings are inherited by every subapp. You can
+# override these settings in the subapps as needed.
+#
+Padrino.configure_apps do
+  # enable :sessions
+  set :session_secret, '21649c158c6f620c944ea5efdab73cdf26bf4eae5b5e93f3e6eb621b4ca31755'
+  set :protection, :except => :path_traversal
+  set :protect_from_csrf, true
+end
+
+# Mounts the core application for this project
+Padrino.mount('Reddit::App', :app_file => Padrino.root('app/app.rb')).to('/')
